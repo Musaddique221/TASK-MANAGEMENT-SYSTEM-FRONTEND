8d98a0b62d5d5150a20196f15fb0e072335fad31
Musaddique Shaikh <musaddiqueshaikh900@gmail.com>
2025-06-09 17:38:39 +0530

    fix vercel issue

--- frontend/src/constants/url.js
+++ frontend/src/constants/url.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://task-management-system-backend-g1db.onrender.com"
